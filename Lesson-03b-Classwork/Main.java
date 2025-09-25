7bb8c86c7f0a6da06f755babe9db62828aad9a26
justinl642 <justinl642@nycstudents.net>
2025-09-25 14:15:07 +0000

    Update

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Justin Li");
+    System.out.println("123 Peach St");
+    System.out.println("Justin Li \n 123 Peach St");
+  }
+
+  
+}
