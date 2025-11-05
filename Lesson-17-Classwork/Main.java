e4c7b0930fee9981c752ab95960202211989c482
justinl642 <justinl642@nycstudents.net>
2025-11-05 15:00:16 +0000

    ee

---
+++ Lesson-17-Classwork/Main.java
@@ -0,0 +1,42 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+
+
+    
+  }
+
+  //will use reverse for problem 3
+  String reverse(String s){
+    String bld="";
+    for(int x=s.length()-1; x>=0; x--)
+      bld+=s.substring(x,x+1);
+
+    return bld;
+  }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(swapLetter("the seen", "e", "%")); 
+    }
+
+    public static String swapLetter(String text, String find, String replace) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            String current = String.valueOf(text.charAt(i));
+            if (current.equals(find)) {
+                result.append(replace);
+            } else {
+                result.append(current);
+            }
+        }
+        return result.toString();
+    }
+}
+
+
+}
